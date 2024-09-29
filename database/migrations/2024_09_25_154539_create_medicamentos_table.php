38f0a55573ea7e285014e825635084eba02523c3
danielmanr <forigua.daniel@gmail.com>
2024-09-28 21:16:30 -0500

    Creacion Base de datos

--- database/migrations/2024_09_25_154539_create_medicamentos_table.php
+++ database/migrations/2024_09_25_154539_create_medicamentos_table.php
@@ -12,7 +12,10 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('medicamentos', function (Blueprint $table) {
-            $table->id();
+            $table->id('Id_Medicamento');
+            $table->string('Nombre');
+            $table->string('Concentracion');
+            $table->string('CodigoBarras');
             $table->timestamps();
         });
     }
